4b7e3a5a00315fd65fab4abcd8d0b4c176f02668
Cyrano <cyrano@arsenalcn.com>
2016-06-08 15:05:39 +0800

    Add json extension of Arsenal.Mobile

---
+++ Arsenal.Mobile/Extensions/JsonRequestExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web;
+
+namespace Arsenal.Mobile.Extensions
+{
+    public static class JsonRequestExtensions
+    {
+        public static bool IsJsonRequest(this HttpRequestBase request)
+        {
+            return string.Equals(request["format"], "json", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ Arsenal.ServiceTests/MyCoupon_Tests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Script.Serialization;
+using Arsenal.Mobile.Models.Casino;
+using Arsenal.Service.Casino;
+using Arsenalcn.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Arsenal.Service.Tests
+{
+    [TestClass]
+    public class MyCouponTests
+    {
+        [TestMethod]
+        public void MyCouponJsonSerializeAndDeserialize_Test()
+        {
+            IRepository repo = new Repository();
+
+            var coupons = repo.Query<CouponView>(x => x.UserID == 443);
+
+            if (coupons != null && coupons.Count > 0)
+            {
+                var couponDtos = coupons.MapToList<CouponView, CouponDto>().ToList();
+
+                var jsonSerializer = new JavaScriptSerializer();
+
+                var json = jsonSerializer.Serialize(couponDtos);
+
+                if (!string.IsNullOrEmpty(json))
+                {
+                    couponDtos = jsonSerializer.Deserialize<List<CouponDto>>(json);
+
+                    Assert.IsInstanceOfType(couponDtos[0], typeof(CouponDto));
+                }
+            }
+        }
+    }
+}

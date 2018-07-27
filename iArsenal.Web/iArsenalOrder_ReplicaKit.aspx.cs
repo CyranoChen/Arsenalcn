ef2b8e7ad988e892a634b81fdcb27a082c5bea02
Cyrano Chen <cyrano@arsenalcn.com>
2018-07-27 21:55:21 +0800

    Add banner of cup kit season 18/19

--- iArsenal.Web/iArsenalOrder_ReplicaKit.aspx.cs
+++ iArsenal.Web/iArsenalOrder_ReplicaKit.aspx.cs
@@ -89,7 +89,7 @@ namespace iArsenal.Web
                 {
                     Page.Title = "阿森纳2018/19赛季杯赛PUMA球衣许愿单";
                     hlReplicaKitPage.NavigateUrl = "https://arsenaldirect.arsenal.com/Football-Shirts-and-Kit/Third/c/third-kit";
-                    ltrlBannerImage.Text = $"<img src=\"uploadfiles/banner/banner20180727.png\" alt=\"{Page.Title}\" />";
+                    ltrlBannerImage.Text = $"<img src=\"uploadfiles/banner/banner20180726.png\" alt=\"{Page.Title}\" />";
                 }
                 else
                 {
